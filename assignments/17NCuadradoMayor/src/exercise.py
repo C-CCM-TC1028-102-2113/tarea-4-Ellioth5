25b74e11291e4fb5af1a3e6b8c14493ca08169c1
Ellioth5 <89115163+Ellioth5@users.noreply.github.com>
2021-09-06 19:22:36 -0500

    Update exercise.py

--- assignments/17NCuadradoMayor/src/exercise.py
+++ assignments/17NCuadradoMayor/src/exercise.py
@@ -2,7 +2,12 @@
 
 def main():
     #Escribe tu código debajo de esta línea
-    pass
+import math
+x=int(input())
+y=math.sqrt(x)
+z=round(y)
+a=z+1
+print(a)
 
 if __name__=='__main__':
     main()
