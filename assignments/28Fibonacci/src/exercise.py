a7e2e7b302770e35411593f479d2b5b6533247eb
Ellioth5 <89115163+Ellioth5@users.noreply.github.com>
2021-09-08 10:47:21 -0500

    Update exercise.py

--- assignments/28Fibonacci/src/exercise.py
+++ assignments/28Fibonacci/src/exercise.py
@@ -1,7 +1,14 @@
 
 def main():
     #escribe tu código abajo de esta línea
-    pass
+a=0
+b=1
+c=0
+x=int(input())
+while c<x:
+    c=c+1
+    a, b=b, a+b
+print(a)
 
 if __name__=='__main__':
     main()
